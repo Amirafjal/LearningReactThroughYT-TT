9b068b28d9ee07a20a0be38cda54e1c626feb589
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2021-09-26 20:29:59 +0530

    learning through YT thapa technical

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  [<h1>Thapa Technical Netflix Pick</h1>,
+  <p>List of 5 Best Series</p>
+  ],
+  document.getElementById('root')
+
+);
