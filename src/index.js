1389d8c79ff4480a61160e5862844a40a0be4834
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2021-09-26 20:48:35 +0530

    challenge 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ var ReactDOM = require('react-dom');
 
 ReactDOM.render(
   [<h1>Thapa Technical Netflix Pick</h1>,
-  <p>List of 5 Best Series</p>
+  <p>List of 5 Best Series</p>,
+  <ol>
+    <li>Dark</li>
+    <li>Extra curricular</li>
+    <li>My Holo Love</li>
+    <li>My first-2 love</li>
+    <li>Mr. Robot</li>
+  </ol>
   ],
   document.getElementById('root')
 
